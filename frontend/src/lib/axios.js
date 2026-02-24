cd1963a708e27a78dde3dccd42f73384b8e237dc
shivamm-1 <chmcs2026.shivam.mallick@gmail.com>
2026-02-24 23:10:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
    // baseURL: 'http://localhost:3000' 
-    baseURL:'https://bookstore-hivg.onrender.com' 
+    baseURL:'https://mern-bookstore-py8c.onrender.com' 
 })
 export default api
